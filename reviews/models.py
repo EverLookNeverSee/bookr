8c1961f7d1515010f041b5b492d217c996cf3ff0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-12 13:33:41 +0430

    Defining book model

--- reviews/models.py
+++ reviews/models.py
@@ -7,3 +7,10 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50, help_text="The name of the publisher.")
     website = models.URLField(help_text="The publisher's website.")
     email = models.EmailField(help_text="The publisher's email address.")
+
+
+class Book(models.Model):
+    """A published book"""
+    title = models.CharField(max_length=70, help_text="The title of the book")
+    publication_date = models.DateField(verbose_name="Date the book was published")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book")
