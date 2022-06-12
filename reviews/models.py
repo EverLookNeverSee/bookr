098abca14b74d6c297450dbc109790cbd295774d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-12 14:31:35 +0430

    Defining model methods

--- reviews/models.py
+++ reviews/models.py
@@ -9,6 +9,9 @@ class Publisher(models.Model):
     website = models.URLField(help_text="The publisher's website.")
     email = models.EmailField(help_text="The publisher's email address.")
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     """A published book"""
@@ -18,6 +21,9 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     contributors = models.ManyToManyField("Contributor", through="BookContributor")
 
+    def __str__(self):
+        return self.title
+
 
 class Contributor(models.Model):
     """
@@ -26,6 +32,9 @@ class Contributor(models.Model):
     first_name = models.CharField(max_length=50, help_text="The contributor's first name or names")
     last_name = models.CharField(max_length=50, help_text="The contributor's last name or names")
     email = models.EmailField(help_text="The contact email for the contributor.")
+
+    def __str__(self):
+        return self.first_name
 
 
 class BookContributor(models.Model):
