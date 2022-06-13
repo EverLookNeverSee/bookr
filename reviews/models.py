3ab93d4327b0fa69ceef6380fe51cc9f91283126
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-13 16:05:35 +0430

    Fixing some minor issues in db models

--- reviews/models.py
+++ reviews/models.py
@@ -29,12 +29,12 @@ class Contributor(models.Model):
     """
         A contributor to a Book, e.g. author, editor, co-author
     """
-    first_name = models.CharField(max_length=50, help_text="The contributor's first name or names")
-    last_name = models.CharField(max_length=50, help_text="The contributor's last name or names")
+    first_names = models.CharField(max_length=50, help_text="The contributor's first name or names")
+    last_names = models.CharField(max_length=50, help_text="The contributor's last name or names")
     email = models.EmailField(help_text="The contact email for the contributor.")
 
     def __str__(self):
-        return self.first_name
+        return self.first_names
 
 
 class BookContributor(models.Model):
