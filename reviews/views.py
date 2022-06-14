937b09c7c7443bf7e2b2c150572d62d8efc99dea
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-14 10:55:51 +0430

    Implementing a simple function-based view

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Book
 
 
 def index(request):
@@ -8,3 +9,9 @@ def index(request):
 def search(request):
     qr = request.GET.get("search")
     return render(request, "search_result.html", {"search_query": qr})
+
+
+def welcome_view(request):
+    message = f"<html><h1>Welcome to Bookr</h1> " \
+              f"<p>{Book.objects.count()} books and counting.</p></html>"
+    return HttpResponse(message)
